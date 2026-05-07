6b1c3fe0757a7069b92704f8b52f6873c8ae064e
jwhiss <JoelJWhissel@gmail.com>
2026-05-06 20:22:13 -0600

    Automatically set displayed host IP

--- display-client/vite.config.js
+++ display-client/vite.config.js
@@ -1,5 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import os from 'os'
+
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return 'localhost';
+}
+
+process.env.VITE_PATRON_URL = `http://${getLocalIP()}:5173`;
 
 // https://vite.dev/config/
 export default defineConfig({
